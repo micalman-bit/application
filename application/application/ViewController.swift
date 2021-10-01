a7a247dd92f7199e34b871d94ff658069834a717
Andrey <micalman@list.ru>
2021-10-01 13:24:46 +0300

    Start project

---
+++ application/application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  application
+//
+//  Created by Andrey Samchenko on 01.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
